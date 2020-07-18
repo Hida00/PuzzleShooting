8d2630389be61416a9202d8f7aa407c66264d3aa
hakuyou00 <kurokage.hi@gmail.com>
2020-07-18 10:37:33 +0900

    Skill add

---
+++ PuzzleShooting/Assets/Script/RateDamageImage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RateDamageImage : MonoBehaviour
+{
+    [NonSerialized]
+    public int num;
+    public int isCenter;
+
+    void Start()
+    {
+        this.GetComponent<Image>().sprite = Resources.Load<Sprite>(@"Image/RateDamage/image" + num.ToString());
+    }
+
+    void Update()
+    {
+        
+    }
+    public void Image_Click()
+    {
+        if(isCenter == 0)
+        {
+            num++;
+            if (num == 4) num = 0;
+
+            Sprite sprite = Resources.Load<Sprite>(@"Image/RateDamage/image" + num.ToString());
+            this.GetComponent<Image>().sprite = sprite;
+        }
+    }
+}
