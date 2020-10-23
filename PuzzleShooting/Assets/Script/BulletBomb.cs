a576d432fb7f76a62a83b0eeeec04d441a33514d
hakuyou00 <kurokage.hi@gmail.com>
2020-10-23 23:19:07 +0900

    commit

---
+++ PuzzleShooting/Assets/Script/BulletBomb.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBomb : MonoBehaviour
+{
+    public GameObject bullet;
+    public Vector3 explosionPos;
+
+    float time;
+
+    void Start()
+    {
+        this.transform.position = Vector3.zero;
+        explosionPos = new Vector3(3 , 3 , 0);
+        time = Time.time + 0.01f;
+    }
+
+    void Update()
+    {
+        float _time = 1f / (Time.time - time);
+        
+        float angle = Mathf.Atan2(explosionPos.y - this.transform.position.y , explosionPos.x - this.transform.position.x) * Mathf.Rad2Deg;
+        if (Vector3.Distance(this.transform.position , explosionPos) > 0.1f)
+        {
+            this.transform.position += new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad) , Mathf.Cos(angle * Mathf.Deg2Rad) , 0) * Time.deltaTime * _time;
+        }
+        else
+        {
+
+        }
+    }
+}
---
+++ PuzzleShooting/Assets/Script/CircleBullet.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleBullet : MonoBehaviour
+{
+    PlayerController _playerController;
+
+    public Image bulletImage;
+    Image img;
+    GameObject canvas;
+
+    public Vector2 scale = new Vector2(0.2f , 0.8f);
+
+    public string imageName = "bullet";
+
+    public float speed;// = 0.1f;
+    public float damagePoint = 1.0f;
+    float moveOverY;
+
+    public bool isBoss = false;
+    public bool isPlayer;
+    public bool isTracking;
+
+    void Start()
+    {
+        canvas = GameObject.Find("BulletPanel");
+        img = Instantiate(bulletImage , canvas.transform);
+        img.GetComponent<Image>().sprite = Resources.Load<Sprite>(@"Image/other/" + imageName);
+        img.rectTransform.localScale = scale;
+        img.rectTransform.rotation = this.transform.rotation;
+        img.transform.position
+            = RectTransformUtility.WorldToScreenPoint(Camera.main , this.transform.position);
+
+        _playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+        moveOverY = ( 19.2f * ( (float)Screen.height / (float)Screen.width ) ) + 1f;
+    }
+
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PLAYER") && !isPlayer)
+        {
+            _playerController.health_Point -= damagePoint - _playerController.defence;
+            Destroy(this.gameObject);
+            Destroy(img);
+        }
+    }
+}
